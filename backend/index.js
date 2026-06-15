90e6ea756de57983cf4bcf98357c7ce0b1fbffbd
shriyanthopu03 <thopu.shriyan@gmail.com>
2026-06-15 18:52:47 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 //////dotenv config/////////////////////
 dotenv.config();
 connectToDB();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 /////////////////middlewares////////////////
 app.use(express.json());
